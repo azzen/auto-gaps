00001fe862a11f1d73437ac4aa12296b7829b2b4
azzen <6373020+Azzen@users.noreply.github.com>
2022-02-14 21:59:46 +0100

    Added telegram notifications

--- hooks/hook.py
+++ hooks/hook.py
@@ -11,7 +11,7 @@ class Hook:
 		if name == 'discord':
 			self._init_discord(config['hooks']['discord']['webhook_url'])
 		elif name == 'telegram':
-			self._init_telegram(config['hooks']['telegram']['token'])
+			self._init_telegram(config['hooks']['telegram']['token'], config['hooks']['telegram']['chat_id'])
 		else:
 			raise ValueError('unknown hook: %s' % name)
 
@@ -41,13 +41,14 @@ class Hook:
 		self.name = 'discord'
 		self.webhook_url = url
 
-	def _init_telegram(self, token):
-		if len(token) == 0:
+	def _init_telegram(self, token, chat_id):
+		if len(token) == 0 or len(chat_id) == 0:
 			self.failed = True
-			log.warn('Skipping telegram hook as no token was specified')
+			log.warn('Skipping telegram hook as no token/chat_id was specified')
 			return
 		self.name = 'telegram'
 		self.token = token
+		self.chat_id = chat_id
 
 	def _format_discord(self):
 		if self.failed: return
@@ -56,12 +57,19 @@ class Hook:
 			field = {'name': f'**{k}** ', 'value': ''}
 			for kk, vv in v.items():
 				if len(vv) > 0:
-					field['value'] += f"{kk.capitalize().ljust(6,'᲼')} : {' '.join([f'` {w} `' for w in vv])}\n"
+					field['value'] += f"{kk.capitalize().ljust(6, '᲼')} : {' '.join([f'` {w} `' for w in vv])}\n"
 			fields.append(field)
 		return fields
 
 	def _format_telegram(self):
-		pass
+		if self.failed: return
+		s = "Vous avez reçu de nouvelles notes sur GAPS !\n"
+		for k, v in self.grades.items():
+			s += f"**{k}**:\n"
+			for kk, vv in v.items():
+				if len(vv) > 0:
+					s += f"{kk.capitalize().ljust(6, ' ')} : {', '.join([f'` {w} `' for w in vv])}\n"
+		return s
 
 	def _notify_discord(self, f):
 		if self.failed: return
@@ -84,4 +92,10 @@ class Hook:
 			log.err('Failed to notify new grades on discord, error: ' + str(res.status_code))
 
 	def _notify_telegram(self, s):
-		pass
+		if self.failed: return
+		request_url = f"https://api.telegram.org/bot{self.token}/sendMessage?chat_id={self.chat_id}&text={s}&parse_mode=markdown"
+		res = requests.get(request_url)
+		if res.status_code < 400:
+			log.success('Successfully notified new grades on Telegram')
+		else:
+			log.err('Failed to notify new grades on Telegram, error: ' + str(res.status_code))
